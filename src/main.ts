2bdd6098202b5df72a7d7835e1d53f03d8d0c4e1
Ali Hassan <alihassankp27@gmail.com>
2022-10-27 19:58:55 +0400

    Initial commit

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,12 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from "vue";
+import Antd from "ant-design-vue";
+import App from "./App.vue";
+import "ant-design-vue/dist/antd.css";
+import router from "./router";
+import { createPinia } from "pinia";
 
-createApp(App).use(router).mount('#app')
+const app = createApp(App);
+app.use(createPinia());
+app.use(router);
+app.use(Antd);
+app.mount("#app");
